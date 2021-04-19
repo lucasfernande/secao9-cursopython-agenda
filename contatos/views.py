d69ef3ac9d22ac70160a8bab81f169151bcce1c7
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-19 16:01:03 -0300

    aplicando paginação

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Contato
 
 
@@ -6,6 +7,12 @@ from .models import Contato
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+
+    contatos = paginator.get_page(page)
+    
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
