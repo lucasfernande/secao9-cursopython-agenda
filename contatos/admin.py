6dee45d201d3e3d71315be74a4ac5c08154afc0d
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-19 14:28:31 -0300

    configurando o django admin

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Categoria, Contato
+
 
 # Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    #  mostra todos os dados na listagem dos contatos no django admin
+
+    list_display_links = ('id', 'nome', 'sobrenome')
+    #  torna os atributos clicáveis
+
+    list_filter = ('nome', 'sobrenome')
+    #  adiciona um campo de filtragem
+
+    list_per_page = 10
+    #  mostra 10 registros por páginas
+
+    search_fields = ('nome', 'sobrenome', 'telefone', 'email', 'categoria')
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome  # alterando a representação da classe no django admin
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -16,3 +19,6 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
