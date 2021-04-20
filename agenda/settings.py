7fbaa3a58c6959014fd02228f0b7a16bb4f56838
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-20 20:48:10 -0300

    campo de imagem

--- agenda/settings.py
+++ agenda/settings.py
@@ -117,6 +117,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 # configurando o django para procurar arquivos estáticos na pasta templates/static
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
