1d9e659b30ace201f62cae7524bcf62d328d761f
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-20 15:07:33 -0300

    campo de pesquisa

--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 from .models import Contato
 
 
@@ -28,4 +30,29 @@ def ver_contato(request, contato_id):
 
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None:
+        raise Http404
+
+    campos = Concat('nome', Value(' '),  'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+
+    paginator = Paginator(contatos, 10)
+
+    page = request.GET.get('p')
+
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
