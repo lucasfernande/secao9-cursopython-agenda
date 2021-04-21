aee6e193679e09fc5131cc12525ce84e3ad8819d
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-21 11:17:56 -0300

    mensagens com django messages

--- contatos/views.py
+++ contatos/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django.core.paginator import Paginator
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 from .models import Contato
 
 
@@ -36,10 +37,16 @@ def ver_contato(request, contato_id):
 def busca(request):
     termo = request.GET.get('termo')
 
-    if termo is None:
-        raise Http404
+    if termo is None or not termo:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Preencha o campo de pesquisa.'
+        )
 
-    campos = Concat('nome', Value(' '),  'sobrenome')
+        return redirect('index')
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
 
     contatos = Contato.objects.annotate(
         nome_completo=campos
